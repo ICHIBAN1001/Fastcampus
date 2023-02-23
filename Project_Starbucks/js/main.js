604018922f83804428e8a5aaef282987a7826de7
ICHIBAN1001 <lji941001@g.yju.ac.kr>
2023-02-24 01:26:09 +0900

    UPDATE : STARBUCKS_HOMEPAGE

--- Project_Starbucks/js/main.js
+++ Project_Starbucks/js/main.js
@@ -62,9 +62,9 @@ fadeEls.forEach(function(fadeEl, index){
 /* 4. NOTICE : 수직 슬라이드(Swiper)  */
 /*   new Swiper ( 선택자 , 옵션)  */ 
 new Swiper('.notice-line .swiper-container' , {
-  direction: 'vertical',
-  autoplay: true,        
-  loop: true 
+  direction: 'vertical', // 수직 슬라이드 
+  autoplay: true,        // 자동 재생 여부
+  loop: true             // 반복 재생 여부
 });
 
 
@@ -72,15 +72,16 @@ new Swiper('.notice-line .swiper-container' , {
 
 /* 5. PROMOTION : 프로모션 이미지 슬라이드(Swiper)  */
 new Swiper('.promotion .swiper-container' ,{
+  // direction: 'horizontal', // 수평 슬라이드
   slidesPerView: 3 , // 한번에 보여줄 슬라이드 개수
   spaceBetween: 10, // 슬라이드 사이 여백
   centeredSlides: true,  //1번 슬라이드가 가운데 보이기  
   loop: true,
-  autoplay : {
-    delay: 5000
+  autoplay: { // 자동 재생 여부
+    delay: 5000 // 5초마다 슬라이드 바뀜
   },
 
-  pagination: {
+  pagination: { // 페이지 번호 사용 여부
     el:'.promotion .swiper-pagination', //페이지 번호 요소 선택자
     clickable : true  // 사용자의 페이지 번호 요소 제어 가능 여부
   },
